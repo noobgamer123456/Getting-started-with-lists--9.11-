0217e51a7b66a9ddb2dff46259ebbdd1b5ba106b
Tabid Hossain <tabid.hossain2009@gmail.com>
2024-09-11 21:29:47 +0600

    first commit

---
+++ Lists.py
@@ -0,0 +1,17 @@
+li = ["Radoya", "Tabid" , "Demure", "Nahian" , "Osama Bin Laden"]
+
+print(li)
+print(li[0])
+print(li[1])
+print(li[2])
+print(li[3])
+print(li[4])
+numbers = [1,2,3,4,5]
+print(numbers)
+print(numbers[-1])
+print(numbers[-2])
+
+aList = [100,200,300,400,500]
+print( aList[::-1] )
+
+
